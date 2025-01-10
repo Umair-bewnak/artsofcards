e98d3f45993a45367761b71b65375d9085ae6e32
Umair Bewnak <umair.bewnak@graymatrix.com>
2025-01-10 12:16:52 +0530

    added carousel data

--- src/data/sliderData.js
+++ src/data/sliderData.js
@@ -11,15 +11,15 @@ const sliderImages = [
     },
     {
       img1: slider2,
-      heading: "Surprise Gift Bags",
+      heading: "Crazy 8’s",
       content:
-        "Explore the Marvel Universe with our remarkable playing cards.Harness the power of your favorite heroes and villains as you play,and let Marvel magic elevate your card games.",
+        "Shuffle Crazy 8’s card game encourages cognitive development, helping kids sharpen their matching, strategic thinking, quick decision-making, and color recognition skills all while also helping you spend quality time with your family, friends and kids. ",
     },
     {
       img1: slider3,
-      heading: "Surprise Gift Bags",
+      heading: "Mickey & Friends",
       content:
-        "Explore the Marvel Universe with our remarkable playing cards.Harness the power of your favorite heroes and villains as you play,and let Marvel magic elevate your card games.",
+        "Learn and Play by Shuffle is a unique 3-piece puzzle that combines numbers, words, colours, and images in a fun and magical way. It also promotes hand-eye coordination, fine motor skills, problem-solving, and critical thinking for your young ones aged 18 months and above.",
     },
   ];
 
